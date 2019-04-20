3393890df347d20ba61fe8a18f24fb7b48a285c0
yash98 <yashmalviya98@gmail.com>
2019-04-20 20:38:51 +0530

    test changed logger

--- test.c
+++ test.c
@@ -3,22 +3,24 @@
 #include "user.h"
 
 int main(void) {
+    scheduler_log_on();
     int c1 = create_container();
     int c2 = create_container();
+
+    ps();
 
     int child1 = fork();
     if (child1==0) {
         join_container(c1);
         ps();
-        ps();
-        printf(1, "child");
+        printf(1, "child\n");
     } else {
         join_container(c2);
         ps();
-        ps();
-        printf(1, "parent");
+        printf(1, "parent\n");
+        scheduler_log_off();
     }
-    printf(1, "reached");
+    printf(1, "reached\n");
 
     exit();
 }
